01a250dfae34e07e384e0ee63e343c5e70007669
dabeann <dabin4078@daum.net>
2023-12-22 11:59:52 +0900

    feat: fun2

--- app/src/androidTest/java/com/example/testapplication/Sample1.kt
+++ app/src/androidTest/java/com/example/testapplication/Sample1.kt
@@ -2,10 +2,16 @@ package com.example.testapplication
 
 fun main() {
     helloWorld()
+
+    println(add(4, 5))
 }
 
 // 1. 함수
 
 fun helloWorld(){
     println("Hello World!")
+}
+
+fun add(a: Int, b: Int) : Int { // : 뒤가 return type
+    return a + b
 }
