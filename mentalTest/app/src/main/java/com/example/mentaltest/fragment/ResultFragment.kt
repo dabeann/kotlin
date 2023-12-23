07dadc6a6c138a9a8f873eae15ac3bf7870c6b9a
dabeann <dabin4078@daum.net>
2023-12-23 21:22:49 +0900

    feat: ResultFragment

--- mentalTest/app/src/main/java/com/example/mentaltest/fragment/ResultFragment.kt
+++ mentalTest/app/src/main/java/com/example/mentaltest/fragment/ResultFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.mentaltest.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +18,59 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var option = -1
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        option = arguments?.getInt("index")?:-1 // 만약 전체가 null이면 -1반환
+
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ResultFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ResultFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        setResult(option)
+
+        val btnHome = view.findViewById<ImageView>(R.id.btn_home)
+        btnHome.setOnClickListener{
+            navController.navigate(R.id.action_resultFragment_to_mainFragment)
+        }
     }
+
+    fun setResult(option: Int) {
+        val tvMain = view?.findViewById<TextView>(R.id.tv_main)
+        val tvSub = view?.findViewById<TextView>(R.id.tv_sub)
+
+        when (option) {
+            1 -> {
+                tvMain?.text = "You are a QUITTER!"
+                tvSub?.text = "You can let the person easily."
+            }
+
+            2 -> {
+                tvMain?.text = "You should focus on yourself."
+                tvSub?.text = "You become really clingy to your ex."
+            }
+
+            3 -> {
+                tvMain?.text = "You should take it easy."
+                tvSub?.text = "You can do crazy things no matter what it takes."
+            }
+
+            4 -> {
+                tvMain?.text = "You are pretty mature."
+                tvSub?.text = "You can easily accept the break-up."
+            }
+        }
+    }
+
 }
