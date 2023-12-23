50ce403361b627523da5f5f2a3d2817f9c002ebf
dabeann <dabin4078@daum.net>
2023-12-23 12:44:47 +0900

    feat: lamda
    - 확장함수
    - Lamda의 Return
    - Lamda를 표현하는 여러가지 방식
    - button.setOnClickListener

--- app/src/androidTest/java/com/example/testapplication/Sample2.kt
+++ app/src/androidTest/java/com/example/testapplication/Sample2.kt
@@ -21,4 +21,53 @@ val nameAge = {name: String, age: Int ->
 fun main() {
     println(square(12))
     println(nameAge("dabin", 22))
+
+    val a = "dabin said"
+    val b = "mac said"
+
+    println(a.pizzaIsGreat())
+    println(b.pizzaIsGreat())
+
+    println(extendString("ariana", 27))
+
+    println(calculateGrade(97))
+    println(calculateGrade(971))
+
+
+    val lamda = {number: Double ->
+        number == 4.3213
+    }
+
+    println(invokeLamda(lamda))
+    println(invokeLamda({it > 3.22}))
+
+    println(invokeLamda { it > 3.22 }) // 마지막 파라미터가 lamda식일 때
 }
+
+// 1-2. 확장함수
+
+val pizzaIsGreat: String.() -> String = {
+    this + "Pizza is the best!"
+}
+
+fun extendString(name: String, age:Int): String{
+    val introduceMyself: String.(Int) -> String ={"I am ${this} and ${it} years old"}
+    return name.introduceMyself(age)
+}
+
+// 1-3. Lamda의 Return
+
+val calculateGrade: (Int) -> String = {
+    when (it) {
+        in 0..40 -> "fail"
+        in 41..70 -> "pass"
+        in 71..100 -> "perfect"
+        else -> "Error"
+    }
+}
+
+// 1-4. Lamda를 표현하는 여러가지 방식
+
+fun invokeLamda(lamda: (Double) -> Boolean) : Boolean {
+    return lamda(5.2343)
+} // Boolean을 return
