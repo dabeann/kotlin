3627928ae6f7341435e0cb39854c13ee4e5bc1fd
dabeann <dabin4078@daum.net>
2023-12-25 12:28:56 +0900

    feat: var

--- testApplication/app/src/androidTest/java/com/example/testapplication/grammar/Grammar1.kt
+++ testApplication/app/src/androidTest/java/com/example/testapplication/grammar/Grammar1.kt
@@ -2,4 +2,10 @@ package com.example.testapplication.grammar
 
 fun main() {
     print("Hello World")
+    println("Hello World!")
+
+    var i: Int = 10
+    var name: String = "dabin"
+    var point: Double = 3.3
+    // 코틀린은 타입을 추론하는 기능을 가지고 있어서 별도로 타입을 지정해 주지 않아도 된다.
 }
